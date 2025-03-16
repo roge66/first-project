c91f23d7ce767da7416cc9fa3c949f5fdd101d3c
Georgiy Dukhovny <stvstvslsl@gmail.com>
2025-03-16 16:14:04 +0300

    Подключены необходимые библиотеки  для того, чтобы у сервиса появился файл Open API и интерфейс Swagger UI;

--- src/UrlShortener/Program.cs
+++ src/UrlShortener/Program.cs
@@ -1,8 +1,28 @@
+using Microsoft.OpenApi.Models;
 using UrlShortener;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UrlShortener API",
+        Version = "v1",
+        Description = "API для сокращения URL-адресов"
+    });
+});
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "UrlShortener API");
+    });
+}
 
 app.MapPost("/shorten", (ShortenUrlRequest request) =>
 {
